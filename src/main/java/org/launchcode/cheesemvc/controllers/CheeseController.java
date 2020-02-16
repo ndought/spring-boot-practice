d9425ae7338ec23e43d9cb19b6b28c7e27b4c159
Noah Doughty <noahdoughty@gmail.com>
2020-02-16 17:38:35 -0500

    dynamic update page working
    finished out controller, can add items to list and added link to take user back to the add page. end vid 2

--- src/main/java/org/launchcode/cheesemvc/controllers/CheeseController.java
+++ src/main/java/org/launchcode/cheesemvc/controllers/CheeseController.java
@@ -3,21 +3,21 @@ package org.launchcode.cheesemvc.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
 
 @Controller
 @RequestMapping("cheese")
 public class CheeseController {
 
+   static ArrayList<String> cheeses = new ArrayList<>();
+
 //   Request path: /cheese
     @RequestMapping(value = "")
     public String index(Model model) {
-
-        ArrayList<String> cheeses = new ArrayList<>();
-        cheeses.add("cheddar");
-        cheeses.add("parmesan");
-        cheeses.add("munster");
 
 
         model.addAttribute("cheeses", cheeses);
@@ -25,6 +25,19 @@ public class CheeseController {
         model.addAttribute("title", "My Cheeses");
         return "cheese/index";
     }
+    @RequestMapping(value = "add", method = RequestMethod.GET)
+    public String displayAddCheeseForm(Model model) {
+        model.addAttribute("title", "Add Cheese");
+        return "cheese/add";
+    }
+    @RequestMapping(value = "add", method = RequestMethod.POST)
+//    requestParam String needs to match line 11 in add.html  page (cheeseName)******
+    public String processAddCheeseForm(@RequestParam String cheeseName) {
+       cheeses.add(cheeseName);
+//       redirect to /cheese
+       return "redirect:";
+    }
+
 }
 
 
