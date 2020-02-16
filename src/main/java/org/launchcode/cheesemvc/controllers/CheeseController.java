0da7ced9b48d48dcdc3b949339740b2a9d1f5829
Noah Doughty <noahdoughty@gmail.com>
2020-02-16 15:53:35 -0500

    start vid 2
    start of video 2, added array ist

--- src/main/java/org/launchcode/cheesemvc/controllers/CheeseController.java
+++ src/main/java/org/launchcode/cheesemvc/controllers/CheeseController.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
+
 @Controller
 @RequestMapping("cheese")
 public class CheeseController {
@@ -11,6 +13,15 @@ public class CheeseController {
 //   Request path: /cheese
     @RequestMapping(value = "")
     public String index(Model model) {
+
+        ArrayList<String> cheeses = new ArrayList<>();
+        cheeses.add("cheddar");
+        cheeses.add("parmesan");
+        cheeses.add("munster");
+
+
+        model.addAttribute("cheeses", cheeses);
+        //        the model attribute is what passes things into the view to view on webpage
         model.addAttribute("title", "My Cheeses");
         return "cheese/index";
     }
